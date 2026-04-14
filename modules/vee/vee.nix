fb2f7ed6af56404665c3847d45053f763e50ee66
Version33 <vee@versionthirtythr.ee>
2026-04-14 19:18:44 -0400

    clean up

--- modules/vee/vee.nix
+++ modules/vee/vee.nix
@@ -7,7 +7,6 @@
         self.packages.${pkgs.stdenv.hostPlatform.system}.fish
       ];
 
-      # Define a user account. Don't forget to set a password with 'passwd'.
       users.users.vee = {
         isNormalUser = true;
         description = "vee";
@@ -36,19 +35,9 @@
             general = {
               renice = 10;
             };
-            # Custom scripts can be added here
-            # gpu = {
-            #   apply_gpu_optimisations = "accept-responsibility";
-            #   gpu_device = 0;
-            #   amd_performance_level = "high";
-            # };
           };
         };
       };
-
-      # Increase runtime directory size for large tmpfs operations (default is 10% of RAM)
-      # Note: Power button behavior is configured in desktop/kde.nix with other logind settings
-      services.logind.settings.Login.RuntimeDirectorySize = "8G";
     };
 
 }
