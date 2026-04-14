bacfd5134ee387bbf9341356752dd60f8726878a
Version33 <vee@versionthirtythr.ee>
2026-04-14 01:44:15 -0400

    clean up

--- modules/desktop/noctalia.nix
+++ modules/desktop/noctalia.nix
@@ -1,11 +1,11 @@
-{ self, inputs, ... }:
+{ inputs, ... }:
 {
   perSystem =
     { pkgs, ... }:
     {
       packages.myNoctalia = inputs.wrapper-modules.wrappers.noctalia-shell.wrap {
         inherit pkgs;
-        settings = (builtins.fromJSON (builtins.readFile ./noctalia.json)).settings;
+        inherit ((builtins.fromJSON (builtins.readFile ./noctalia.json))) settings;
       };
     };
 }
