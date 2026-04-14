bde8e8a0c64d29d42859fc67e70d64dbe31f2517
Version33 <vee@versionthirtythr.ee>
2026-04-14 02:23:38 -0400

    lg theme

--- modules/wrapped/lazygit.nix
+++ modules/wrapped/lazygit.nix
@@ -4,8 +4,8 @@
     { pkgs, ... }:
     let
       themeFile = builtins.fetchurl {
-        url = "https://raw.githubusercontent.com/catppuccin/lazygit/main/themes/mocha/blue.yml";
-        sha256 = "1kzq15bsd384bws3z2k1x3r742gwkiva0680a1ylx7glw02jizvf";
+        url = "https://raw.githubusercontent.com/catppuccin/lazygit/main/themes-mergable/mocha/blue.yml";
+        sha256 = "1a8ccxzcka396bzslllqk81n1kwkggk5hi4pl3rv865v1qhzc7k5";
       };
     in
     {
