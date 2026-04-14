b0af5779881311ff871ba71880f05a8edcc7221f
Version33 <vee@versionthirtythr.ee>
2026-04-13 22:18:56 -0400

    yazi update

--- modules/wrapped/yazi.nix
+++ modules/wrapped/yazi.nix
@@ -1,33 +1,25 @@
 { ... }:
-let
-  mkYazi =
-    pkgs:
-    pkgs.yazi.override {
-      plugins = {
-        "wl-clipboard" = pkgs.yaziPlugins.wl-clipboard;
-      };
-      extraPackages = [ pkgs.wl-clipboard ];
-      settings.keymap = {
-        manager.keymap = [
-          {
-            on = "<C-y>";
-            run = "plugin wl-clipboard";
-            desc = "Copy to clipboard";
-          }
-        ];
-      };
-    };
-in
 {
-  perSystem =
-    { pkgs, ... }:
-    {
-      packages.yazi = mkYazi pkgs;
-    };
-
   flake.modules.nixos.wrapped-yazi =
     { pkgs, ... }:
     {
-      environment.systemPackages = [ (mkYazi pkgs) ];
+      environment.systemPackages = [
+        (pkgs.yazi.override {
+          extraPackages = with pkgs; [
+            ffmpeg # video thumbnails
+            _7zz # archive extraction and preview
+            jq # JSON preview
+            poppler-utils # PDF preview
+            fd # file searching
+            ripgrep # file content searching
+            fzf # quick file subtree navigation
+            zoxide # historical directory navigation
+            resvg # SVG preview
+            imagemagick # font, HEIC, and JPEG XL preview
+            chafa # image preview in terminal
+            wl-clipboard # clipboard support on Wayland
+          ];
+        })
+      ];
     };
 }
