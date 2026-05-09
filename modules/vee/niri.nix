00f4c94ef5589303712c5755b6b9b26e4d51452a
Version33 <vee@versionthirtythr.ee>
2026-05-08 21:08:35 -0400

    electron title bar color

--- modules/vee/niri.nix
+++ modules/vee/niri.nix
@@ -9,7 +9,10 @@
         package = self.packages.${pkgs.stdenv.hostPlatform.system}.myNiri;
       };
 
-      environment.systemPackages = [ pkgs.catppuccin-cursors.mochaDark ];
+      environment.systemPackages = with pkgs; [
+        catppuccin-cursors.mochaDark
+        adw-gtk3
+      ];
 
       # XWayland apps (e.g. Steam) read XCURSOR_* env vars rather than the
       # niri compositor cursor setting, so we need both.
@@ -20,6 +23,35 @@
         XCURSOR_THEME = "catppuccin-mocha-dark-cursors";
         XCURSOR_SIZE = "24";
         XCURSOR_PATH = lib.mkForce "${pkgs.catppuccin-cursors.mochaDark}/share/icons:~/.icons:~/.local/share/icons";
+        GTK_THEME = "adw-gtk3-dark";
+      };
+
+      # Force dark theme preference so Electron/Wayland apps use a dark
+      # title bar instead of white. Also override headerbar colors for
+      # XWayland apps to Catppuccin Mocha base (#1e1e2e).
+      environment.etc = {
+        "gtk-3.0/settings.ini".text = ''
+          [Settings]
+          gtk-theme-name=adw-gtk3-dark
+          gtk-application-prefer-dark-theme=1
+        '';
+        "gtk-3.0/gtk.css".text = ''
+          headerbar {
+            background-color: #1e1e2e;
+            color: #cdd6f4;
+          }
+        '';
+        "gtk-4.0/settings.ini".text = ''
+          [Settings]
+          gtk-theme-name=adw-gtk3-dark
+          gtk-application-prefer-dark-theme=1
+        '';
+        "gtk-4.0/gtk.css".text = ''
+          headerbar {
+            background-color: #1e1e2e;
+            color: #cdd6f4;
+          }
+        '';
       };
 
       services.greetd = {
