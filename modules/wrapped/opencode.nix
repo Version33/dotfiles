634bfb6de859096eddc87ed82ce4de65732f1ac0
Version33 <vee@versionthirtythr.ee>
2026-05-11 00:22:49 -0400

    opencode mcp servers

--- modules/wrapped/opencode.nix
+++ modules/wrapped/opencode.nix
@@ -3,15 +3,39 @@
   perSystem =
     { pkgs, ... }:
     let
-      tuiConfig = pkgs.writeText "tui.json" (builtins.toJSON {
-        theme = "catppuccin";
-      });
+      tuiConfig = pkgs.writeText "tui.json" (
+        builtins.toJSON {
+          theme = "catppuccin";
+        }
+      );
+      opencodeConfig = pkgs.writeText "opencode.json" (
+        builtins.toJSON {
+          mcp = {
+            context7 = {
+              type = "remote";
+              url = "https://mcp.context7.com/mcp";
+            };
+            gh_grep = {
+              type = "remote";
+              url = "https://mcp.grep.app";
+            };
+            sentry = {
+              type = "remote";
+              url = "https://mcp.sentry.dev/mcp";
+              oauth = { };
+            };
+          };
+        }
+      );
     in
     {
       packages.opencode = inputs.wrapper-modules.lib.wrapPackage {
         inherit pkgs;
         package = pkgs.opencode;
-        envDefault.OPENCODE_TUI_CONFIG = toString tuiConfig;
+        envDefault = {
+          OPENCODE_TUI_CONFIG = toString tuiConfig;
+          OPENCODE_CONFIG = toString opencodeConfig;
+        };
       };
     };
 }
