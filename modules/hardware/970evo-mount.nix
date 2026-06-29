6757666b3baf487b8e98a12f66b82694b8a4ca7e
vee <v33@versionthirtythr.ee>
2026-06-29 09:09:35 -0400

    make module

--- modules/hardware/970evo-mount.nix
+++ modules/hardware/970evo-mount.nix
@@ -1,10 +1,12 @@
 {
-  fileSystems."/mnt/970evo" = {
-    device = "/dev/disk/by-uuid/861ff253-c1de-4674-9613-b9eb76d9c6b6";
-    fsType = "ext4";
-    options = [
-      "noatime"
-      "nofail"
-    ];
+  flake.modules.nixos.amdgpu = {
+    fileSystems."/mnt/970evo" = {
+      device = "/dev/disk/by-uuid/861ff253-c1de-4674-9613-b9eb76d9c6b6";
+      fsType = "ext4";
+      options = [
+        "noatime"
+        "nofail"
+      ];
+    };
   };
 }
