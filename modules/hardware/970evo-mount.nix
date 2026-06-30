099d19504695f0314a1752a758278d75ff6172e5
vee <v33@versionthirtythr.ee>
2026-06-29 22:23:17 -0400

    change module name for ssd mounting

--- modules/hardware/970evo-mount.nix
+++ modules/hardware/970evo-mount.nix
@@ -1,5 +1,5 @@
 {
-  flake.modules.nixos.amdgpu = {
+  flake.modules.nixos.storage = {
     fileSystems."/mnt/970evo" = {
       device = "/dev/disk/by-uuid/861ff253-c1de-4674-9613-b9eb76d9c6b6";
       fsType = "ext4";
