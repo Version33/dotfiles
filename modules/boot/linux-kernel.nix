9064344c6077b863e61029eff0a3dd1d51071fda
vee <v33@versionthirtythr.ee>
2026-07-07 08:37:29 -0400

    maybe fix ethernet turning off sometimes

--- modules/boot/linux-kernel.nix
+++ modules/boot/linux-kernel.nix
@@ -14,6 +14,8 @@
           # "usbcore.autosuspend=-1"
           # Shorten USB enumeration timeout to reduce boot delay from broken internal port (usb3-7)
           "usbcore.initial_descriptor_timeout=5"
+          # Disable ASPM on r8169 to prevent RTL8126 NIC sleep/wake issues
+          "r8169.aspm=0"
         ];
       };
 
