245b63622fb9097d903eddb16c5fc931b5a21eeb
Version33 <vee@versionthirtythr.ee>
2026-08-24 17:01:02 -0400

    change system action prio

--- modules/wrapped/noctalia.nix
+++ modules/wrapped/noctalia.nix
@@ -6,6 +6,13 @@
       packages.noctalia = inputs.wrapper-modules.wrappers.noctalia-shell.wrap {
         inherit pkgs;
         inherit ((builtins.fromJSON (builtins.readFile ./noctalia.json))) settings;
+        # Upstream demotes session actions below app results (`score - 1`)
+        package = pkgs.noctalia-shell.overrideAttrs (old: {
+          postPatch = (old.postPatch or "") + ''
+            substituteInPlace Modules/Panels/Launcher/Providers/SessionProvider.qml \
+              --replace-fail '"_score": score - 1,' '"_score": score + 1,'
+          '';
+        });
       };
     };
 }
